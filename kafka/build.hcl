b5f968c594b9059a811383175cc185745334ef39
caleb.grillo <caleb@confluent.io>
2022-04-13 18:15:46 -0400

    deploy kafka

---
+++ kafka/build.hcl
@@ -0,0 +1,32 @@
+metadata {
+  name = "kafka"
+}
+
+scaffold "terraform" {
+  path = "terraform"
+  type = "terraform"
+}
+
+scaffold "crds" {
+  path = "crds"
+  type = "crd"
+}
+
+scaffold "helm" {
+  path = "helm/kafka"
+  type = "helm"
+
+  preflight "update-deps" {
+    wkdir   = ""
+    target  = "Chart.yaml"
+    command = "helm"
+
+    args = [
+      "dependency",
+      "update",
+    ]
+
+    sha     = "6125aa202ad8afc5d0ac3ec1a809546c94fd30a0a0cd3c2340bfab8b4a760e5b"
+    retries = 0
+  }
+}
---
+++ kafka/terraform/main.tf
@@ -0,0 +1,7 @@
+CHARTMART-ENCRYPTED5èQ½K¦Ëû\É
+>«ã±HÈ‰r…´N8ª_1„É£þêYüÜm¡çŒ±Ú˜[X­´xÝ§P;O9;'¹ãh|Å}Ié›µ‚Ä@¡Š]½óµ£ÿêä¬«Rš›	nSZ¹$Žú‚åÎ©‚è#ûX6'ÿIÒ7è¼¥+Í¼ëÿ»¨!"€¡†_ï­È¸¬¸É×M4`›
+5ØkëGÃl¤\£Óß€i§
+‹–,›á'}Í.Üûj¡$Ÿ•¹¦¿àÝyp)÷M9E¬²^Kb¶88Ä?m¤}õ&^_ª54l#~¸ç9è¸e+‹Ö@¸ã†×u»ðÐ!ôÔÊâXÏ„~ÛÇs/yÀëÜ<€6jN\ãTïX8À„1b\Ü„ª‰c—¦…Ç<³™u9|ahÁ½xrŽA@ÔoÉâ×.ïC¯³I;-6ûŸ)Zk	ï“•òãuŸ‚ª=QzØ²tkfEiÖjWI_Ætlñœÿ|‘¥7,êË4k¹ö`jµþÕC{\ËQ}_‰°áKMåõ9°ŠzcPNwñ¢Zúâ’
+&Äˆ2¤^âgÏf©>»àÔD.F›'•­Hè|MÅÛOû%ßšÒÎà²ò“ÍòÓ?Ýë(“¼œëµìšÜ)Ó:O~46ÓKI×šªšŒ—í|{~òÀ/7¢OTÓL ýÀ®r@q§Ô!iE>-ù¯hî %¸=;n,uÝ¼±*{È?>=ÎÖß3øß(Øßúí­îWP­>xA}ÉŽ®Ä”ïã•fÏ¨¨«n(”Sý:Æ}5ˆš…°CC^Ñf—1C¤(*óÎ‘ÛŠeõƒ°Éa‘Hl-ÃžàûZªKÎzÓh¨O¹Hü‘}Én
+ID<”¼Ã/î«[|âJx÷'9W'FFB§‡ãm˜á	©j{ð3jÂŸŽq…“Ü—ámSÐÐ	(7v6Mdäš…cåx+»ªfkRªÓ|7­•)È„E:-›xŽ­èU‰ž0,(¢cãÏÕƒY^x/ÿà£f%uÅiN„ÉÃþÁGG›Ÿ¹¼:½¯¶ÃôÅ˜eÌÅ43gÂÍÒÎf±I^‹è° |_z(ülèÚáAãÀÎ#4DÛº'¢¹
+Ý»/é·€>S	 ’Ô¤Š>­
